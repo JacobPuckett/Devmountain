327e30bd7ac6d4047625877c2fe425f05383c4c4
Jacob Puckettgit config --global user.email jjp10015@gmail.com <jjp10015@gmail.com>
2023-01-25 14:58:19 -0700

    commit

---
+++ week_1/day_2/maxOfTwo.js
@@ -0,0 +1,11 @@
+let x = 1;
+let y = 2;
+
+
+if(x > y){
+    console.log(`the largest value is ${x}`);
+}else if(y > x){
+    console.log(`the largest value is ${y}`);
+}else{
+    console.log(`the values, ${y} and ${x}, are identical.`);
+}
